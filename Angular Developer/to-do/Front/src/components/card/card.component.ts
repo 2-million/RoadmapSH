b787b6583baecb8dd2faa0f5fe3534f5844446c3
Dumilin V.V. <dumilinvadim@gmail.com>
2025-04-14 22:11:46 +0300

    init new typescript proj

--- Angular Developer/to-do/Front/src/components/card/card.component.ts
+++ Angular Developer/to-do/Front/src/components/card/card.component.ts
@@ -1,16 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-card',
-  imports: [ButtonModule, CheckboxModule],
+  imports: [ButtonModule, CheckboxModule, CommonModule, FormsModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
+  providers: [CookieService],
   standalone: true,
 })
 export class CardComponent {
-  showAlert() {
-    alert('Button clicked!');
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = new Array();
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === 'done' ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    if (description != '' && description.length < 200)
+      this.allItems.push({
+        id: Date.now(),
+        description,
+        done: false,
+      });
+  }
+
+  deleteItem(item: any) {
+    this.allItems = this.allItems.filter((i) => i.id !== item.id);
+  }
+
+  updateItem(item: any) {
+    const newDescription = prompt('edit task:', item.description);
+    if (newDescription !== null && newDescription.trim()) {
+      item.description = newDescription.trim();
+    }
+  }
+
+  toggleDone(item: any) {
+    item.done = !item.done;
   }
 }
